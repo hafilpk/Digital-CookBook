3414df7ace3121693946ce560184d33c4534b189
hafilpk <pkmshafil@gmail.com>
2025-09-17 08:43:22 +0530

    add JWT token and endpoints

--- backend/cookbook/urls.py
+++ backend/cookbook/urls.py
@@ -24,6 +24,10 @@ from api.views import (
     MealPlanViewSet,
     MealPlanEntryViewSet,
 )
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 router = routers.DefaultRouter()
 router.register(r"ingredients", IngredientViewSet)
@@ -36,4 +40,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
     path("api-auth/", include("rest_framework.urls")),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
